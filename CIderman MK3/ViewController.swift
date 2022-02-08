24932971c2e115a7137adf97b18001bd7577e245
AimiKomatsu <ai-komatsu@kddi-tech.com>
2022-02-08 10:56:06 +0900

    Initial Commit

---
+++ CIderman MK3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CIderman MK3
+//
+//  Created by komatsuaimi on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
